d274050a5587a0aeda7e0f80b2991fb38d72293a
Jus-Jar <jaredheeralal04@gmail.com>
2023-04-11 13:48:02 -0400

    updated database URL

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgres://justgymdb_user:cEURuLkAcNe9D8KoGx3BWkWkNvFDpoow@dpg-cgfqibg2qv28tc0qm49g-a/justgymdb"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
