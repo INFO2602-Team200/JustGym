a8f00485fea6a3e471a05c515ac801ae58d9ae76
Jus-Jar <jaredheeralal04@gmail.com>
2023-04-12 00:17:18 -0400

    db url

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://justgymdb_user:cEURuLkAcNe9D8KoGx3BWkWkNvFDpoow@dpg-cgfqibg2qv28tc0qm49g-a/justgymdb"
 SECRET_KEY = "secret key"
 JWT_ACCESS_TOKEN_EXPIRES = 7
 ENV = "DEVELOPMENT"
