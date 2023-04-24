d5039265b38fc20f3fc109f98930feab6b0ddb49
kimberly-moses1 <itskimmybruh@gmail.com>
2023-04-24 02:23:40 +0000

    fix config file

--- App/config.py
+++ App/config.py
@@ -1,6 +1,6 @@
-# SQLALCHEMY_DATABASE_URI = "postgresql://justgymdb_user:cEURuLkAcNe9D8KoGx3BWkWkNvFDpoow@dpg-cgfqibg2qv28tc0qm49g-a.oregon-postgres.render.com/justgymdb"
-# SECRET_KEY = "secret key"
-# JWT_ACCESS_TOKEN_EXPIRES = 7
-# ENV = "DEVELOPMENT"
+SQLALCHEMY_DATABASE_URI = "postgresql://justgymdb_user:cEURuLkAcNe9D8KoGx3BWkWkNvFDpoow@dpg-cgfqibg2qv28tc0qm49g-a.oregon-postgres.render.com/justgymdb"
+SECRET_KEY = "secret key"
+JWT_ACCESS_TOKEN_EXPIRES = 7
+ENV = "DEVELOPMENT"
 
 # postgres://justgymdb_user:cEURuLkAcNe9D8KoGx3BWkWkNvFDpoow@dpg-cgfqibg2qv28tc0qm49g-a.oregon-postgres.render.com/justgymdb
